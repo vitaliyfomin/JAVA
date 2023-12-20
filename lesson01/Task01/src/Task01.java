05269bc8bb0ca9a12af2cedee33280b0315d0e7b
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-12-20 20:56:14 +0500

    ...

--- lesson01/Task01/src/Task01.java
+++ lesson01/Task01/src/Task01.java
@@ -1,26 +1,26 @@
 class Answer {
-    public int countNTriangle(int n) {
-        // Введите свое решение ниже
+    public int countNTriangle(int n){
+      // Введите свое решение ниже
 
     }
 }
 
 // Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
-public class Printer {
-    public static void main(String[] args) {
-        
-        int n = 0;
-
-        if (args.length == 0) {
-            // При отправке кода на Выполнение, вы можете варьировать эти параметры
-            n = 4;
-        } else {
-            n = Integer.parseInt(args[0]);
-        }
-
+public class Printer{ 
+    public static void main(String[] args) { 
+      int n = 0;
+      
+      if (args.length == 0) {
+        // При отправке кода на Выполнение, вы можете варьировать эти параметры
+        n = 4;
+      }
+      else{
+        n = Integer.parseInt(args[0]);
+      }     
+      
         // Вывод результата на экран
-        Answer ans = new Answer();
-        int itresume_res = ans.countNTriangle(n);
-        System.out.println(itresume_res);
+      Answer ans = new Answer(); 
+      int itresume_res = ans.countNTriangle(n);     
+      System.out.println(itresume_res);
     }
 }
