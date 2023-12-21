a3c7e790b8103595679f73d620e30cb9ca243622
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-12-21 19:26:21 +0500

    Task01

--- lesson01/Task01/src/Task01.java
+++ lesson01/Task01/src/Task01.java
@@ -1,26 +1,29 @@
 class Answer {
-    public int countNTriangle(int n){
-      // Введите свое решение ниже
-
+  public int countNTriangle(int n) {
+    if (n < 1) {
+      return -1; // Если число n < 1 (ненатуральное), возвращаем -1
     }
+
+    // Используем формулу для вычисления треугольных чисел: Tn = n * (n + 1) / 2
+    return n * (n + 1) / 2;
+  }
 }
 
 // Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
-public class Printer{ 
-    public static void main(String[] args) { 
-      int n = 0;
-      
-      if (args.length == 0) {
-        // При отправке кода на Выполнение, вы можете варьировать эти параметры
-        n = 4;
-      }
-      else{
-        n = Integer.parseInt(args[0]);
-      }     
-      
-        // Вывод результата на экран
-      Answer ans = new Answer(); 
-      int itresume_res = ans.countNTriangle(n);     
-      System.out.println(itresume_res);
+public class Task01 {
+  public static void main(String[] args) {
+    int n = 0;
+
+    if (args.length == 0) {
+      // При отправке кода на Выполнение, вы можете варьировать эти параметры
+      n = 4;
+    } else {
+      n = Integer.parseInt(args[0]);
     }
+
+    // Вывод результата на экран
+    Answer ans = new Answer();
+    int result = ans.countNTriangle(n);
+    System.out.println(result);
+  }
 }
