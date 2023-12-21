2b6db0eb9b910ae2e42089e26aa28c7267fb5955
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-12-21 19:51:14 +0500

    Task02

---
+++ lesson01/Task02/src/Task0202.java
@@ -0,0 +1,29 @@
+class Answer {
+    public void printPrimeNums() {
+        for (int i = 2; i <= 1000; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class Task0202 {
+    public static void main(String[] args) {
+        Answer ans = new Answer();
+        ans.printPrimeNums();
+    }
+}
