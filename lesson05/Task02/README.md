2120572e7ac9482277127d4a409306698461eff0
Vitaliy Fomin <fomin_v.v@mail.ru>
2024-01-17 23:15:00 +0500

    Lesson05: Task02

--- lesson05/Task02/README.md
+++ lesson05/Task02/README.md
@@ -1,18 +1,18 @@
-## Getting Started
+## Подсчет повторяющихся имен
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Вы работаете с приложением для учета имен пользователей. Ваша задача - разработать программу, которая принимает на вход пять имен пользователей (или использует имена по умолчанию, если аргументы не предоставлены) и подсчитывает, сколько раз каждое имя встречается.
+Программа должна использовать **HashMap** для хранения имен и их количества вхождений.
+По завершении, программа выводит результат в виде пар "имя - количество".
 
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+На входе:
+```
+'Elena'
+'Elena'
+'Elena'
+'Ivan'
+'Ivan'
+```
+На выходе:
+```
+{Ivan=2, Elena=3}
+```
--- lesson05/Task02/src/Task02.java
+++ lesson05/Task02/src/Task02.java
@@ -1,5 +1,53 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.HashMap;
+
+class NamesCounter {
+    private HashMap<String, Integer> names = new HashMap<>();
+
+    // Метод для добавления имени в HashMap
+    public void addName(String name) {
+        // Получаем текущее количество вхождений имени
+        Integer count = names.getOrDefault(name, 0);
+        // Увеличиваем количество на 1 и обновляем в HashMap
+        names.put(name, count + 1);
+    }
+
+    // Метод для вывода содержимого HashMap
+    public void showNamesOccurrences() {
+        // Выводим содержимое HashMap
+        System.out.println(names);
+    }
+}
+
+// Не удаляйте и не меняйте метод Main! 
+public class Task02 {
+    public static void main(String[] args) {
+        String name1;
+        String name2;
+        String name3;
+        String name4;
+        String name5;
+      
+        if (args.length == 0) {
+            name1 = "Elena";
+            name2 = "Elena";
+            name3 = "Elena";
+            name4 = "Elena";
+            name5 = "Ivan";
+        } else {
+            name1 = args[0];
+            name2 = args[1];
+            name3 = args[2];
+            name4 = args[3];
+            name5 = args[4];
+        }      
+        NamesCounter namesCounter = new NamesCounter();
+
+        namesCounter.addName(name1);
+        namesCounter.addName(name2);
+        namesCounter.addName(name3);
+        namesCounter.addName(name4);
+        namesCounter.addName(name5);
+
+        namesCounter.showNamesOccurrences();
     }
 }
